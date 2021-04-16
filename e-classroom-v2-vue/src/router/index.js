0e8ee194babba100d13df22da58cd92bfb47be73
Lemur <danilo_jevtovic@hotmail.com>
2021-04-16 22:21:49 +0200

    dodate putanje za login/register

--- e-classroom-v2-vue/src/router/index.js
+++ e-classroom-v2-vue/src/router/index.js
@@ -1,18 +1,30 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+
+import Login from "../views/auth/Login.vue";
+import Signup from "../views/auth/Signup.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
-  
-]
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/signup",
+    name: "Signup",
+    component: Signup,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
