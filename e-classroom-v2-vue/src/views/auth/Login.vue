7f39270641f18d5f5b26c561df7df01aa5945399
Lemur <danilo_jevtovic@hotmail.com>
2021-04-16 22:19:41 +0200

    login/signup forme

---
+++ e-classroom-v2-vue/src/views/auth/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <form>
+    <h2>Login</h2>
+    <input type="email" placeholder="email" />
+    <input type="password" placeholder="password" />
+    <button>Login</button>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ e-classroom-v2-vue/src/views/auth/Signup.vue
@@ -0,0 +1,18 @@
+<template>
+  <form>
+    <h2>Signup</h2>
+    <input type="text" placeholder="Email" />
+    <input type="text" placeholder="Password" />
+    <input type="text" placeholder="First Name" />
+    <input type="text" placeholder="Last Name" />
+    <input type="text" placeholder="Phone" />
+    <button>Signup</button>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
