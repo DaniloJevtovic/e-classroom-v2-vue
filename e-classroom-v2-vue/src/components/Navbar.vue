a710aac3b3bd51440d6667e7b56208b3e8c1714b
Lemur <danilo_jevtovic@hotmail.com>
2021-04-16 22:21:37 +0200

    css za navbar

--- e-classroom-v2-vue/src/components/Navbar.vue
+++ e-classroom-v2-vue/src/components/Navbar.vue
@@ -1,13 +1,56 @@
 <template>
-  
+  <div class="navbar">
+    <nav>
+      <img src="@/assets/logo.png" />
+      <h2>e-classroom</h2>
+      <div class="links">
+        <router-link :to="{ name: 'Login' }">Login</router-link>
+        <router-link :to="{ name: 'Signup' }">Signup</router-link>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
+<style scoped>
+.navbar {
+  padding: 16px 10px;
+  margin-bottom: 50px;
+  background: white;
+}
 
+nav {
+  display: flex;
+  align-items: center;
+  margin: 0 auto;
+}
+
+nav h2 {
+  margin-left: 10px; /*od lijeve ivice 20px */
+}
+
+nav .links {
+  margin-left: auto; /*ovo ih poravna udesno?! */
+}
+
+nav .links a,
+button {
+  margin-left: 10px; /*razmak izmedju dugmica */
+  font-size: 20px;
+}
+
+.links a.router-link-exact-active {
+  background: cyan;
+  color: indigo;
+  padding: 10px;
+  font-weight: bold;
+  border-radius: 10px;
+}
+
+nav img {
+  max-height: 30px;
+}
 </style>
