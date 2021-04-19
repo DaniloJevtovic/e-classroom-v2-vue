f735d74eac7c879ec6b12dea2a58a7bb3d7857d5
Lemur <danilo_jevtovic@hotmail.com>
2021-04-19 22:12:11 +0200

    login/logout /generisanje linkova

--- e-classroom-v2-vue/src/store/index.js
+++ e-classroom-v2-vue/src/store/index.js
@@ -1,12 +1,102 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import axios from "axios";
+import router from "../router/index.js";
 
 export default createStore({
   state: {
+    loggedUser: null,
+    linksForLoggedUser: [],
+
+    adminLinks: ["AdminHome", "AllUsers", "AllCourses", "AllScClasses"],
+    teacherLinks: ["TeacherHome"],
+    studentLinks: ["StudentHome"],
+  },
+  getters: {
+    getLoggedUser(state) {
+      return state.loggedUser;
+    },
+    getLinksForLoggedUser(state) {
+      return state.linksForLoggedUser;
+    },
   },
   mutations: {
+    parseTokenAndSaveUser(state) {
+      let token = localStorage.getItem("token");
+      console.log("token storage", token);
+
+      if (token != null) {
+        var base64Payload = token.split(".")[1];
+        var payload = Buffer.from(base64Payload, "base64");
+
+        let user = JSON.parse(payload.toString());
+        localStorage.setItem("user", JSON.stringify(user.user));
+
+        state.loggedUser = user.user;
+      }
+    },
+
+    setLinksForLoggedUser(state) {
+      if (state.loggedUser) {
+        if (state.loggedUser.authorities[0].authority === "ROLE_ADMIN") {
+          state.linksForLoggedUser = state.adminLinks;
+          router.push({ name: "AdminHome" });
+        } else if (
+          state.loggedUser.authorities[0].authority === "ROLE_TEACHER"
+        ) {
+          state.linksForLoggedUser = state.teacherLinks;
+          router.push({ name: "TeacherHome" });
+        } else if (
+          state.loggedUser.authorities[0].authority === "ROLE_STUDENT"
+        ) {
+          state.linksForLoggedUser = state.studentLinks;
+          router.push({ name: "StudentHome" });
+        } else {
+          state.loggedUserLinks = [];
+          router.push({ name: "Login" });
+        }
+      }
+    },
+
+    logout(state) {
+      state.loggedUser = null;
+      state.linksForLoggedUser = [];
+      localStorage.clear();
+      router.push({ name: "Login" });
+    },
   },
   actions: {
+    async login(context, payload) {
+      try {
+        const response = await axios.post(
+          "http://localhost:5555/auth/login/",
+          JSON.stringify(payload),
+          {
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        localStorage.setItem("token", response.data.accessToken);
+
+        context.commit("parseTokenAndSaveUser");
+        context.commit("setLinksForLoggedUser");
+
+        console.log(response);
+      } catch (err) {
+        console.log("greska", err.message);
+      }
+    },
+
+    loginRefresh(context) {
+      context.commit("parseTokenAndSaveUser");
+      context.commit("setLinksForLoggedUser");
+    },
+
+    logout(context) {
+      context.commit("logout");
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
