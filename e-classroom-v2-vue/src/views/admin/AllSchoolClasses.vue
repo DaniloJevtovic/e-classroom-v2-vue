40ebad5749f2dab2f735a5959225f60957c56cb1
Lemur <danilo_jevtovic@hotmail.com>
2021-04-20 02:31:26 +0200

    komp. za prikaz svih razreda, odjeljenja i predmeta

---
+++ e-classroom-v2-vue/src/views/admin/AllSchoolClasses.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <h2>All SchoolClasses</h2>
+    {{ scClasses }}
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from "vue";
+import useCRUD from "../../composables/useCRUD.js";
+
+export default {
+  setup() {
+    const scClasses = ref([]);
+    const { getAll } = useCRUD();
+
+    const getAllScClasses = async () => {
+      scClasses.value = await getAll("scClasses");
+    };
+
+    onMounted(getAllScClasses);
+
+    return { scClasses };
+  },
+};
+</script>
+
+<style>
+</style>
