90f42deb4c9154acea9abcf3edbf4d7960cee9f0
Lemur <danilo_jevtovic@hotmail.com>
2021-04-21 13:27:54 +0200

    prikaz svih predmeta za odredjenog profesora

---
+++ e-classroom-v2-vue/src/views/courses/TeacherCourses.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <div class="container-header">
+      <h2>My Courses</h2>
+    </div>
+    <div class="container-body">
+      <div class="courses" v-for="course in courses" :key="course.id">
+        <router-link :to="{ name: 'CourseDetails', params: { id: course.id } }">
+          <button>
+            Name: {{ course.name }}
+            <hr />
+            Teacher: {{ course.teacher.firstName }}
+            {{ course.teacher.lastName }}
+            <hr />
+            Description: {{ course.description }}
+          </button>
+        </router-link>
+        <!-- <hr /> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from "vue";
+import { useStore } from "vuex";
+import useCRUD from "../../composables/useCRUD.js";
+
+export default {
+  setup() {
+    const store = useStore();
+    const { getSubItems } = useCRUD();
+
+    const courses = ref([]);
+    const getCourses = async () => {
+      let teacher = store.getters["getLoggedUser"];
+      courses.value = await getSubItems("courses", "teacher", teacher.id);
+    };
+
+    onMounted(getCourses);
+    return { courses };
+  },
+};
+</script>
+
+<style>
+.courses {
+  display: inline;
+}
+</style>
