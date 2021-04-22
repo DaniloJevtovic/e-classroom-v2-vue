5f82a5e8f47238f8b957461965d408489b6c9b14
Lemur <danilo_jevtovic@hotmail.com>
2021-04-22 14:16:38 +0200

    ucitavanje svih ucenika na predmetu

--- e-classroom-v2-vue/src/views/courses/teacher/MyCourseDetails.vue
+++ e-classroom-v2-vue/src/views/courses/teacher/MyCourseDetails.vue
@@ -5,17 +5,30 @@
         Name: {{ courseInfo.name }}
         <br />
         Description: {{ courseInfo.description }}
+        <br />
+        SchoolClass: {{ courseInfo.schoolClass.name }}
       </h2>
-       <router-link :to="{ name: 'MyCourses' }">
-      <button>Back to my Courses</button>
-    </router-link>
+      <router-link :to="{ name: 'MyCourses' }">
+        <button>Back to my Courses</button>
+      </router-link>
     </div>
     <div class="container-body">
       <h2>Materials</h2>
       <button>New Material</button>
-    </div>
 
-   
+      <hr />
+      <h2>Quizzes</h2>
+      <button>New Quiz</button>
+
+      <hr />
+      <h2>Students</h2>
+      <!-- za predmet nadjes kojem razredu pripada, zatim sa tim id om nadjes sva odjeljenja -->
+      <div class="students" v-for="student in students" :key="student.id">
+        <button>{{ student.firstName }} {{ student.lastName }}</button>
+      </div>
+
+    
+    </div>
   </div>
 </template>
 
@@ -27,15 +40,23 @@ export default {
   props: ["id"],
   setup(props) {
     const courseInfo = ref("");
-    const { getById } = useCRUD();
+    const students = ref([]);
+    const { getById, getSubItems } = useCRUD();
 
     const getCourseDetails = async () => {
       courseInfo.value = await getById("courses", props.id);
     };
 
-    onMounted(getCourseDetails);
+    const getStudentsFromCourse = async () => {
+      students.value = await getSubItems("students", "course", props.id);
+    };
 
-    return { courseInfo };
+    onMounted(() => {
+      getCourseDetails();
+      getStudentsFromCourse();
+    });
+
+    return { courseInfo, students };
   },
 };
 </script>
