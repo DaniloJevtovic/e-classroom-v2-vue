3912d4ee94d624567687128babfe58f2481dab77
Lemur <danilo_jevtovic@hotmail.com>
2021-04-24 21:32:58 +0200

    kreiranje kviza i detalji

---
+++ e-classroom-v2-vue/src/views/quizzes/teacher/NewQuiz.vue
@@ -0,0 +1,50 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <h2>New Quiz</h2>
+    <input type="text" v-model="newQuiz.name" placeholder="name" required />
+    <textarea
+      rows="3"
+      v-model="newQuiz.instructions"
+      placeholder="instructions"
+    ></textarea>
+    <input
+      type="number"
+      v-model="newQuiz.duration"
+      placeholder="duration"
+      required
+    />
+    <button>Create</button>
+    <button @click="$router.go(-1)">Cancel</button>
+  </form>
+</template>
+
+<script>
+import { reactive } from "vue";
+import useCRUD from "@/composables/useCRUD.js";
+import { useRouter } from "vue-router";
+
+export default {
+  props: ["id"],
+  setup(props) {
+    const { save } = useCRUD();
+    const router = useRouter();
+
+    const newQuiz = reactive({
+      name: "",
+      instructions: "",
+      duration: "",
+      courseId: props.id,
+    });
+
+    const handleSubmit = async () => {
+      console.log(newQuiz);
+      await save("quizzes", newQuiz);
+      router.go(-1);
+    };
+    return { newQuiz, handleSubmit };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ e-classroom-v2-vue/src/views/quizzes/teacher/QuizDetails.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <div class="container-header">
+      <h3>Details</h3>
+      <button @click="$router.go(-1)">Back</button>
+    </div>
+    <div class="container-body">
+      <h3>Name: {{ quizInfo.name }}</h3>
+      <h3>Instructions: {{ quizInfo.instructions }}</h3>
+      <h3>Duration: {{ quizInfo.duration }}</h3>
+      <hr />
+
+      <h3>Questions</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from "vue";
+import useCRUD from "@/composables/useCRUD.js";
+
+export default {
+  props: ["id"],
+  setup(props) {
+    const { getById } = useCRUD();
+    const quizInfo = ref("");
+
+    const getQuiz = async () => {
+      quizInfo.value = await getById("quizzes", props.id);
+    };
+
+    onMounted(() => {
+      getQuiz();
+    });
+
+    return { quizInfo };
+  },
+};
+</script>
+
+<style>
+</style>
