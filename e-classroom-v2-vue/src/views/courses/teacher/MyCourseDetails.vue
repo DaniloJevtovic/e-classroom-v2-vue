672aa0f9d8b616f2613e6673217d38beffdda307
Lemur <danilo_jevtovic@hotmail.com>
2021-04-27 22:30:38 +0200

    male izmjene

--- e-classroom-v2-vue/src/views/courses/teacher/MyCourseDetails.vue
+++ e-classroom-v2-vue/src/views/courses/teacher/MyCourseDetails.vue
@@ -42,7 +42,7 @@
         <h2>Name: {{ quiz.name }}</h2>
         <h3>Instruction: {{ quiz.instructions }}</h3>
         <h4>Duration: {{ quiz.duration }} minutes.</h4>
-        <router-link :to="{ name: 'QuizDetails', params: { quizId: quiz.id } }">
+        <router-link :to="{ name: 'QuizDetails', params: {id: id, quizId: quiz.id } }">
           <button>details</button>
         </router-link>
         <hr />
