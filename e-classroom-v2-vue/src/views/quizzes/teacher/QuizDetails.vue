b98609bfff8b39c5a6355c0d940c2d855fbca6ef
Lemur <danilo_jevtovic@hotmail.com>
2021-04-27 22:30:29 +0200

    ucitavanje svih pitanja za kviz

--- e-classroom-v2-vue/src/views/quizzes/teacher/QuizDetails.vue
+++ e-classroom-v2-vue/src/views/quizzes/teacher/QuizDetails.vue
@@ -2,8 +2,10 @@
   <div class="container">
     <div class="container-header">
       <h3>Details</h3>
-      <!-- vracam za 2 -->
-      <button @click="$router.go(-2)">Back</button> 
+      <!-- <button @click="$router.go(-2)">Back</button> -->
+      <router-link :to="{ name: 'MyCourseDetails', params: { id: id } }">
+        <button>Back to course</button>
+      </router-link>
     </div>
     <div class="container-body">
       <h3>Name: {{ quizInfo.name }}</h3>
@@ -12,6 +14,17 @@
       <hr />
 
       <h3>Questions</h3>
+      <router-link :to="{ name: 'NewQuestion', params: { quizId: quizId } }">
+        <button>New Question</button>
+      </router-link>
+
+      <div
+        class="question-div"
+        v-for="question in questions"
+        :key="question.id"
+      >
+        <p>Question: {{ question.question }}</p>
+      </div>
     </div>
   </div>
 </template>
@@ -21,20 +34,31 @@ import { ref, onMounted } from "vue";
 import useCRUD from "@/composables/useCRUD.js";
 
 export default {
-  props: ["quizId"],
+  props: ["id", "quizId"],
   setup(props) {
-    const { getById } = useCRUD();
+    const { getById, getSubItems } = useCRUD();
     const quizInfo = ref("");
+
+    const questions = ref([]);
 
     const getQuiz = async () => {
       quizInfo.value = await getById("quizzes", props.quizId);
     };
 
+    const getQuestionsForQuiz = async () => {
+      questions.value = await getSubItems("questions", "quiz", props.quizId);
+    };
+
+    const newQuestion = () => {
+      questions.value.push("ss");
+    };
+
     onMounted(() => {
       getQuiz();
+      getQuestionsForQuiz();
     });
 
-    return { quizInfo };
+    return { quizInfo, questions, newQuestion };
   },
 };
 </script>
