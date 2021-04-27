3351d83a6909dee6630cd11a410354e16441fcad
Lemur <danilo_jevtovic@hotmail.com>
2021-04-27 22:30:14 +0200

    forma za novo pitanje

---
+++ e-classroom-v2-vue/src/views/quizzes/teacher/NewQuestion.vue
@@ -0,0 +1,46 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <h2>New Question</h2>
+    <textarea
+      rows="8"
+      v-model="question.question"
+      placeholder="enter question"
+    ></textarea>
+    <input type="number" v-model="question.points" placeholder="points" />
+    <button>Save</button>
+  </form>
+</template>
+
+<script>
+import useCRUD from "@/composables/useCRUD.js";
+import { reactive } from "vue";
+import { useRouter } from "vue-router";
+
+export default {
+  props: ["quizId"],
+  setup(props) {
+    const { save } = useCRUD();
+    const router = useRouter();
+
+    const question = reactive({
+      question: "",
+      points: "",
+      quizId: props.quizId,
+    });
+
+    const handleSubmit = async () => {
+      await save("questions", question);
+      console.log(question);
+      router.push({ name: "QuizDetails", params: { quizId: props.quizId } });
+    };
+    return { question, handleSubmit };
+  },
+};
+</script>
+
+<style scoped>
+form {
+  background: cyan;
+  max-width: 500px;
+}
+</style>
