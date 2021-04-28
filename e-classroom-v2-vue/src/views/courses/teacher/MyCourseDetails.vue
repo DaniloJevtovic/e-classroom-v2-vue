65f12803dbf86099d3846c45f9c5f5c5e2205c4f
Lemur <danilo_jevtovic@hotmail.com>
2021-04-28 20:36:44 +0200

    prikaz svih kvizova koji nisu obrisani

--- e-classroom-v2-vue/src/views/courses/teacher/MyCourseDetails.vue
+++ e-classroom-v2-vue/src/views/courses/teacher/MyCourseDetails.vue
@@ -39,13 +39,18 @@
       </router-link>
 
       <div class="quizzes" v-for="quiz in quizzes" :key="quiz.id">
-        <h2>Name: {{ quiz.name }}</h2>
-        <h3>Instruction: {{ quiz.instructions }}</h3>
-        <h4>Duration: {{ quiz.duration }} minutes.</h4>
-        <router-link :to="{ name: 'QuizDetails', params: {id: id, quizId: quiz.id } }">
-          <button>details</button>
-        </router-link>
-        <hr />
+        <!-- samo kvizovi koji nisu obrisani -->
+        <div v-if="quiz.status !== 'DELETED'">
+          <h2>Name: {{ quiz.name }}</h2>
+          <h3>Instruction: {{ quiz.instructions }}</h3>
+          <h4>Duration: {{ quiz.duration }} minutes.</h4>
+          <router-link
+            :to="{ name: 'QuizDetails', params: { id: id, quizId: quiz.id } }"
+          >
+            <button>details</button>
+          </router-link>
+          <hr />
+        </div>
       </div>
 
       <hr />
