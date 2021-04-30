b1e0d2835369ae620507a2555b63fe442b32acc0
Lemur <danilo_jevtovic@hotmail.com>
2021-04-30 03:39:56 +0200

    komponenta sa korisnicima - admin

---
+++ e-classroom-v2-vue/src/views/users/Users.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <div class="container-header">
+      <child-navbar :links="usersLinks"></child-navbar>
+    </div>
+    <div class="container-body">
+       <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import ChildNavbar from "../../components/ChildNavbar.vue";
+
+export default {
+  components: { ChildNavbar },
+  setup() {
+    const usersLinks = ref([
+      { name: "All Users", path: "AllUsers" },
+      { name: "All Teachers", path: "AllTeachers" },
+      { name: "All Students", path: "AllStudents" },
+    ]);
+    return { usersLinks };
+  },
+};
+</script>
+
+<style>
+</style>
