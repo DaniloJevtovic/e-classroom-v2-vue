e849519c31ef4547c0673391d14a35221fc3b400
Lemur <danilo_jevtovic@hotmail.com>
2021-04-30 03:37:41 +0200

    svi razredi za predmet - profesor

---
+++ e-classroom-v2-vue/src/views/courses/teacher/MyStudentClasses.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="my-stClass-info">
+    <div class="my-stClasses" v-for="stClass in stClasses" :key="stClass.id">
+      <div class="my-stClass">
+        <h2>Student class{{ stClass.name }}</h2>
+        <h3>Students</h3>
+        <my-students :stClassId="stClass.id"></my-students>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from "vue";
+import useCRUD from "../../../composables/useCRUD.js";
+import MyStudents from "./MyStudents.vue";
+
+export default {
+  props: ["id"],
+  components: { MyStudents },
+  setup(props) {
+    const { getSubItems } = useCRUD();
+    const stClasses = ref([]);
+
+    const getStudentClassesForCourse = async () => {
+      stClasses.value = await getSubItems("stClasses", "course", props.id);
+    };
+
+    onMounted(() => {
+      getStudentClassesForCourse();
+    });
+
+    return { stClasses };
+  },
+};
+</script>
+
+<style scoped>
+.my-stClass-info {
+  background: chartreuse;
+  padding: 20px;
+}
+.my-stClasses {
+  background: orangered;
+  padding: 20px;
+}
+.my-stClass {
+  border: 3px solid yellow;
+}
+.my-stClass h2 {
+    padding: 10px;
+}
+</style>
