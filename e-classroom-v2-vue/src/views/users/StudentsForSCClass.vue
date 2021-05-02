4dff9990aa51e4a7c48a036835beae99fefb59f3
Lemur <danilo_jevtovic@hotmail.com>
2021-05-02 03:08:23 +0200

    ucenici iz razreda u posebnoj komponenti

---
+++ e-classroom-v2-vue/src/views/users/StudentsForSCClass.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="stsForClass">
+    <router-link :to="{ name: 'NewStudentForStClass', params: { id } }">
+      <button>Add new Student</button>
+    </router-link>
+    <div>
+      <div v-for="(student, index) in students" :key="student.id">
+      <div>
+        <h3>
+          {{ index + 1 }}. Name: {{ student.firstName }} {{ student.lastName }}
+        </h3>
+      </div>
+    </div>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from "vue";
+import useCRUD from "../../composables/useCRUD.js";
+
+export default {
+  props: ["id"],
+  setup(props) {
+    const { getSubItems } = useCRUD();
+    const students = ref([]);
+
+    const getStudentsForStClass = async () => {
+      students.value = await getSubItems("students", "stClass", props.id);
+    };
+
+    onMounted(() => {
+      getStudentsForStClass();
+    });
+
+    return { students };
+  },
+};
+</script>
+
+<style scoped>
+.stsForClass {
+  background: oldlace;
+}
+</style>
