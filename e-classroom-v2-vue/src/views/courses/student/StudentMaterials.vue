499367bdbffb19db4a333a28b5b81ddd0060585b
Lemur <danilo_jevtovic@hotmail.com>
2021-05-03 06:56:49 +0200

    materijali za studente i detalji materijala

---
+++ e-classroom-v2-vue/src/views/courses/student/StudentMaterials.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="materials-info">
+    <h2>Materials</h2>
+
+    <div class="basic-grid">
+      <div class="materials" v-for="material in materials" :key="material.id">
+        <h2>Name: {{ material.name }}</h2>
+        <h3>Description: {{ material.description }}</h3>
+        <router-link
+          :to="{ name: 'StudentMaterialDetails', params: { matId: material.id } }"
+        >
+          <button>details</button>
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from "vue";
+import useCRUD from "../../../composables/useCRUD.js";
+
+export default {
+  props: ["id"],
+  setup(props) {
+    const materials = ref([]);
+
+    const { getById, getSubItems } = useCRUD();
+
+    const getMaterialsForCourse = async () => {
+      materials.value = await getSubItems("materials", "course", props.id);
+    };
+
+    onMounted(() => {
+      getMaterialsForCourse();
+    });
+
+    return { materials };
+  },
+};
+</script>
+
+<style scoped>
+.materials-info {
+  padding: 10px;
+  background: rgb(27, 27, 184);
+}
+
+.materials {
+  color: white;
+  border: 2px solid pink;
+  border-radius: 10px;
+  background: deepskyblue;
+  padding: 8px;
+}
+</style>
---
+++ e-classroom-v2-vue/src/views/materials/student/StudentMaterialDetails.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <div class="container-header">
+      <button @click="$router.go(-1)">Back to materials</button>
+    </div>
+    <div class="container-body">
+      <h3>Name: {{ materialInfo.name }}</h3>
+      <h3>Description: {{ materialInfo.description }}</h3>
+
+      <hr />
+
+      <h3>Files</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from "vue";
+import useCRUD from "@/composables/useCRUD.js";
+
+export default {
+  props: ["id"],
+  setup(props) {
+    const { getById } = useCRUD();
+    const materialInfo = ref("");
+
+    const getMaterial = async () => {
+      materialInfo.value = await getById("materials", props.id);
+    };
+
+    onMounted(() => {
+      getMaterial();
+    });
+
+    return { materialInfo };
+  },
+};
+</script>
+
+<style>
+</style>
