a9779785f497329ac094a7b3e7ae7bab9224240f
Lemur <danilo_jevtovic@hotmail.com>
2021-05-05 02:03:06 +0200

    izmjena razreda

---
+++ e-classroom-v2-vue/src/views/schoolClasses/EditSchoolClass.vue
@@ -0,0 +1,51 @@
+<template>
+  <form>
+    <h2>Edit School Class</h2>
+    <input type="text" v-model="scClass.name" placeholder="name" required />
+    <textarea
+      rows="4"
+      v-model="scClass.description"
+      placeholder="description"
+      required
+    >
+    </textarea>
+    <button @click.prevent="handleSubmit">Save</button>
+    <button @click.prevent="$router.go(-1)">Cancel</button>
+  </form>
+</template>
+
+<script>
+import { onMounted, reactive } from "vue";
+import { useRouter } from "vue-router";
+import useCRUD from "../../composables/useCRUD.js";
+
+export default {
+  props: ["id"],
+  setup(props) {
+    const { getById, editById } = useCRUD();
+    const router = useRouter();
+
+    const scClass = reactive({});
+
+    const getScClass = async () => {
+      let sc = await getById("scClasses", props.id);
+      scClass.name = sc.name;
+      scClass.description = sc.description;
+    };
+
+    onMounted(() => {
+      getScClass();
+    });
+
+    const handleSubmit = async () => {
+      await editById("scClasses", props.id, scClass);
+      router.go(-1);
+    };
+
+    return { scClass, handleSubmit };
+  },
+};
+</script>
+
+<style>
+</style>
