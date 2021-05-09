9e4191801bdc7be8f1dba5fa2559a452a34c06bd
Lemur <danilo_jevtovic@hotmail.com>
2021-05-09 20:49:12 +0200

    prikaz fajlova za materijal kod ucenika - download

--- e-classroom-v2-vue/src/views/materials/student/StudentMaterialDetails.vue
+++ e-classroom-v2-vue/src/views/materials/student/StudentMaterialDetails.vue
@@ -10,6 +10,12 @@
       <hr />
 
       <h3>Files</h3>
+
+      <div v-for="file in files" :key="file.id">
+        <button @click.prevent="downFile(file)">
+          <h3>{{ file.name }}</h3>
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -17,22 +23,35 @@
 <script>
 import { ref, onMounted } from "vue";
 import useCRUD from "@/composables/useCRUD.js";
+import useUpDownFile from "@/composables/useUpDownFile.js";
 
 export default {
-  props: ["id"],
+  props: ["id", "matId"],
   setup(props) {
-    const { getById } = useCRUD();
+    const { getById, getSubItems } = useCRUD();
+    const { downloadFile } = useUpDownFile();
+
     const materialInfo = ref("");
+    const files = ref([]);
 
     const getMaterial = async () => {
       materialInfo.value = await getById("materials", props.id);
     };
 
+    const getFilesForMaterial = async () => {
+      files.value = await getSubItems("files", "material", props.matId);
+    };
+
     onMounted(() => {
       getMaterial();
+      getFilesForMaterial();
     });
 
-    return { materialInfo };
+    const downFile = async (file) => {
+      await downloadFile(file);
+    };
+
+    return { materialInfo, files, downFile };
   },
 };
 </script>
