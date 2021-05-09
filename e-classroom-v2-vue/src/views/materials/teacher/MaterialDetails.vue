3b7ca950e8f19b785a851bdbc07c4bff5d8060ca
Lemur <danilo_jevtovic@hotmail.com>
2021-05-09 16:35:18 +0200

    ucitavane svih fajlova za materijal

--- e-classroom-v2-vue/src/views/materials/teacher/MaterialDetails.vue
+++ e-classroom-v2-vue/src/views/materials/teacher/MaterialDetails.vue
@@ -10,6 +10,12 @@
       <hr />
 
       <h3>Files</h3>
+
+      <div v-for="file in files" :key="file.id">
+        <button @click.prevent="downloadFile(file.id)">
+          <h3>{{ file.name }}</h3>
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -17,22 +23,50 @@
 <script>
 import { ref, onMounted } from "vue";
 import useCRUD from "@/composables/useCRUD.js";
+import axios from "axios";
 
 export default {
-  props: ["id"],
+  props: ["matId"],
   setup(props) {
-    const { getById } = useCRUD();
+    const { getById, getSubItems } = useCRUD();
     const materialInfo = ref("");
+    const files = ref([]);
 
     const getMaterial = async () => {
-      materialInfo.value = await getById("materials", props.id);
+      materialInfo.value = await getById("materials", props.matId);
+    };
+
+    const getFilesForMaterial = async () => {
+      files.value = await getSubItems("files", "material", props.matId);
     };
 
     onMounted(() => {
       getMaterial();
+      getFilesForMaterial();
     });
 
-    return { materialInfo };
+    const downloadFile = async (id) => {
+      console.log(id);
+      try {
+        const response = await axios.get(
+          "http://localhost:5555/api/files/downloadFile/" + id,
+          {
+            headers: {
+              Authorization: "Bearer " + localStorage.getItem("token"),
+            },
+          }
+        );
+
+        console.log(response);
+
+        return response.data;
+      } catch (err) {
+        console.log(err);
+        return err;
+      }
+    };
+
+    return { materialInfo, files, downloadFile };
   },
 };
 </script>
