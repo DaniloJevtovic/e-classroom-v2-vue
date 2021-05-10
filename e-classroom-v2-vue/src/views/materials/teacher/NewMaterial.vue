e8db658e9316abba529deb0692d5488987f454f5
Lemur <danilo_jevtovic@hotmail.com>
2021-05-10 14:05:47 +0200

    dodavanje fajlova za novi materijal

--- e-classroom-v2-vue/src/views/materials/teacher/NewMaterial.vue
+++ e-classroom-v2-vue/src/views/materials/teacher/NewMaterial.vue
@@ -8,42 +8,85 @@
       placeholder="Description"
     >
     </textarea>
-    <input type="file" placeholder="select" />
+
+    <div class="mat-files">
+      <input type="file" @change="addToList($event)" placeholder="select" />
+      <p v-for="(file, index) in files" :key="file">
+        {{ file.name }} <button @click.prevent="deleteFile(index)">delete</button>
+      </p>
+    </div>
 
     <button>Save</button>
-    <button @click="$router.go(-1)">Cancel</button>
+    <button @click.prevent="$router.go(-1)">Cancel</button>
   </form>
 </template>
 
 <script>
-import { reactive } from "vue";
+import { reactive, ref } from "vue";
 import { useRouter } from "vue-router";
 
 import useCRUD from "@/composables/useCRUD.js";
+import useUpDownFile from "@/composables/useUpDownFile.js";
 
 export default {
   props: ["id"],
   setup(props) {
     const { save } = useCRUD();
-
+    const { uploadFile } = useUpDownFile();
     const router = useRouter();
+    
     const newMaterial = reactive({
       name: "",
       description: "",
-      file: "",
       courseId: props.id,
     });
 
+    const files = ref([]);
+
     const handleSubmit = async () => {
-      await save("materials", newMaterial);
+      //cuvam materijal
+      let res = await save("materials", newMaterial);
+
+      //upload fajlova za taj materjal
+      await uploadFiles(res.id);
+
       console.log(newMaterial);
       router.go(-1);
     };
 
-    return { newMaterial, handleSubmit };
+    const addToList = (event) => {
+      //dodajem fajlove u listu
+      console.log(event.target.files[0]);
+      files.value.push(event.target.files[0]);
+    };
+
+    const deleteFile = (index) => {
+      files.value.splice(index, 1);
+    };
+
+    const uploadFiles = async (matId) => {
+      console.log(matId);
+
+      for (let i = 0; i < files.value.length; i++) {
+        console.log(files.value[i].name);
+
+        let formData = new FormData();
+        formData.append("file", files.value[i], files.value[i].name);
+
+        await uploadFile(matId, formData);
+      }
+
+    };
+
+    return { newMaterial, handleSubmit, addToList, files, deleteFile };
   },
 };
 </script>
 
-<style>
+<style scoped>
+.mat-files {
+  border: 1px solid indigo;
+  padding: 8px;
+  margin-bottom: 10px;
+}
 </style>
