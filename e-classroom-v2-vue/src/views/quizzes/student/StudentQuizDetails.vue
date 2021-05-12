f0d1fe3b6da20ea259a1d8ab5389cddf34a4f46c
Lemur <danilo_jevtovic@hotmail.com>
2021-05-12 14:48:31 +0200

    ucitavanje rezultata kviza

--- e-classroom-v2-vue/src/views/quizzes/student/StudentQuizDetails.vue
+++ e-classroom-v2-vue/src/views/quizzes/student/StudentQuizDetails.vue
@@ -5,14 +5,12 @@
       <h3>Instruction: {{ quizInfo.instructions }}</h3>
       <h4>Duration: {{ quizInfo.duration }}</h4>
 
-      <router-link
-        v-if="true"
-        :to="{ name: 'StudentSolveQuiz', params: { quizId: quizId } }"
-      >
-        <button>Start</button>
-      </router-link>
+      <div v-if="result">
+        <student-quiz-results :result="result"></student-quiz-results>
+      </div>
+
       <div v-else>
-        <student-quiz-results></student-quiz-results>
+        <button @click.prevent="startQuiz">Start</button>
       </div>
     </div>
   </div>
@@ -21,6 +19,8 @@
 <script>
 import { ref, onMounted } from "vue";
 import useCRUD from "@/composables/useCRUD.js";
+import { useRouter } from "vue-router";
+import { useStore } from "vuex";
 import StudentQuizResults from "./StudentQuizResults.vue";
 
 export default {
@@ -29,18 +29,50 @@ export default {
     StudentQuizResults,
   },
   setup(props) {
-    const { getById } = useCRUD();
+    const { getById, getSubSubItems, save } = useCRUD();
+    const router = useRouter();
+    const store = useStore();
+
+    const student = store.getters["getLoggedUser"];
     const quizInfo = ref("");
+    const result = ref("");
 
     const getQuizInfo = async () => {
       quizInfo.value = await getById("quizzes", props.quizId);
     };
 
+    const getResults = async () => {
+      result.value = await getSubSubItems(
+        "results",
+        "student",
+        "quiz",
+        student.id,
+        props.quizId
+      );
+    };
+
     onMounted(() => {
       getQuizInfo();
+      getResults();
     });
 
-    return { quizInfo };
+    const startQuiz = async () => {
+      let newResult = {
+        studentId: student.id,
+        quizId: props.quizId,
+        points: 0,
+      };
+
+      const res = await save("results", newResult);
+      console.log("kreirao: ", res.id);
+
+      router.push({
+        name: "StudentSolveQuiz",
+        params: { quizId: props.quizId, stRes: res.id },
+      });
+    };
+
+    return { quizInfo, result, startQuiz };
   },
 };
 </script>
