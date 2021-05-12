57c42fcbef5b07c0cf78b0eb6f89f96b2f69bdcd
Lemur <danilo_jevtovic@hotmail.com>
2021-05-12 23:47:05 +0200

    premjesteno u drugi folder

---
+++ e-classroom-v2-vue/src/views/quizzes/student/res/StudentQuizResults.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <h2>Results</h2>
+    <h2>Points: {{ result.points }}</h2>
+
+    <student-res-questions
+      :quizId="result.quiz.id"
+      :resId="result.id"
+    ></student-res-questions>
+  </div>
+</template>
+
+<script>
+import StudentResQuestions from "./StudentResQuestions.vue";
+
+export default {
+  props: ["result"],
+  components: { StudentResQuestions },
+  setup() {},
+};
+</script>
+
+<style>
+</style>
