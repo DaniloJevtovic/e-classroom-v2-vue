c116194a5e3e55b0dffaa97064dfbbf572660fc7
Lemur <danilo_jevtovic@hotmail.com>
2021-05-16 21:53:15 +0200

    lista ucenika za roditelja

---
+++ e-classroom-v2-vue/src/views/users/parent/ParentStudentsList.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+    <div class="parent-stList">
+      <h1>My students</h1>
+      <div v-for="student in students" :key="student.id">
+        <div>
+          <router-link
+            :to="{ name: 'ParentStDetails', params: { id: student.id } }"
+          >
+            <h1>{{ student.firstName }} {{ student.lastName }}</h1>
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from "vue";
+import { useStore } from "vuex";
+import useCRUD from "../../../composables/useCRUD.js";
+
+export default {
+  setup() {
+    const { getSubItems } = useCRUD();
+    const store = useStore();
+
+    const students = ref([]);
+
+    const getParentStudents = async () => {
+      let parent = store.getters["getLoggedUser"];
+      students.value = await getSubItems("students", "parent", parent.id);
+    };
+
+    onMounted(() => {
+      getParentStudents();
+    });
+
+    return { students };
+  },
+};
+</script>
+
+<style scoped>
+.parent-stList {
+  background: oldlace;
+}
+</style>
