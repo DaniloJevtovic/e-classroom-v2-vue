04db9a74697eedd483d1c5f6c4d726295792ff6c
Lemur <danilo_jevtovic@hotmail.com>
2021-05-17 18:24:30 +0200

    komponenta za poruke - container za primljene i poslate

---
+++ e-classroom-v2-vue/src/views/messages/Messages.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <div class="container-header">
+      <child-navbar :links="links"></child-navbar>
+    </div>
+
+    <router-view />
+  </div>
+</template>
+
+<script>
+import ChildNavbar from "../../components/ChildNavbar.vue";
+
+export default {
+  components: { ChildNavbar },
+  setup() {
+    const links = [
+      { name: "Recived messages", path: "RecivedMessages" },
+      { name: "Sent Messages", path: "SentMessages" },
+    ];
+
+    return { links };
+  },
+};
+</script>
+
+<style>
+</style>
