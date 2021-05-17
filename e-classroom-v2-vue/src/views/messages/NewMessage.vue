5e7d1d7365bc7e1bc0e9cc5e4305b8d6e061513d
Lemur <danilo_jevtovic@hotmail.com>
2021-05-17 18:23:22 +0200

    forma za novu poruku

---
+++ e-classroom-v2-vue/src/views/messages/NewMessage.vue
@@ -0,0 +1,67 @@
+<template>
+  <form @submit.prevent="sendMessage">
+    <h2>New Message</h2>
+    <h2 v-if="reciver">
+      Reciver: {{ reciver.firstName }} {{ reciver.lastName }}
+    </h2>
+
+    <input
+      type="text"
+      v-model="message.subject"
+      placeholder="subject"
+      required
+    />
+    <textarea rows="4" v-model="message.message" placeholder="message" required>
+    </textarea>
+    <button>Send</button>
+    <button @click.prevent="$router.go(-1)">Cancel</button>
+  </form>
+</template>
+
+<script>
+import { ref, onMounted, reactive } from "vue";
+import useCRUD from "@/composables/useCRUD.js";
+import { useRouter, useRoute } from "vue-router";
+import { useStore } from "vuex";
+
+export default {
+  props: ["senderId, reciverId"],
+  setup(props) {
+    const { getById, save } = useCRUD();
+    const store = useStore();
+    const router = useRouter();
+    const route = useRoute();
+
+    const loggedUser = store.getters["getLoggedUser"];
+
+    const message = reactive({
+      subject: "",
+      message: "",
+      //senderId: route.params.senderId,
+      senderId: loggedUser.id,
+      reciverId: route.params.reciverId,
+    });
+
+    const reciver = ref("");
+
+    const getReciver = async () => {
+      reciver.value = await getById("users", route.params.reciverId);
+    };
+
+    const sendMessage = async () => {
+      await save("messages", message);
+      router.go(-1);
+    };
+
+    onMounted(() => {
+      console.log(route.params.senderId);
+      getReciver();
+    });
+
+    return { message, sendMessage, reciver };
+  },
+};
+</script>
+
+<style>
+</style>
