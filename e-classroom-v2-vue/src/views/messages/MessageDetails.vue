f7e143719ad85ea4a6ec1291c6844bbe066de8b0
Lemur <danilo_jevtovic@hotmail.com>
2021-05-18 14:30:57 +0200

    detalji poruke sa mogucnoscu odgovora

---
+++ e-classroom-v2-vue/src/views/messages/MessageDetails.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container">
+    <div class="container-header">
+      <button @click="$router.go(-1)">Back</button>
+    </div>
+    <div class="container-body">
+      <h2>Message details</h2>
+      <div class="message-details">
+        <h2>Subject: {{ message.subject }}</h2>
+        <p>{{ message.message }}</p>
+      </div>
+      <div class="replay-message">
+        <textarea
+          rows="10"
+          v-model="reply.message"
+          placeholder="replay"
+        ></textarea>
+        <button @click.prevent="sendReply">Send</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import useCRUD from "@/composables/useCRUD.js";
+import { useRouter } from "vue-router";
+import { onMounted, ref, reactive } from "vue";
+
+export default {
+  props: ["messId"],
+  setup(props) {
+    const { getById, save } = useCRUD();
+    const router = useRouter();
+
+    const message = ref("");
+
+    const reply = reactive({ message: "" });
+
+    const getMessage = async () => {
+      let mess = await getById("messages", props.messId);
+
+      message.value = mess;
+      reply.subject = "RE: " + mess.subject;
+      reply.senderId = mess.sender.id;
+      reply.reciverId = mess.reciver.id;
+    };
+
+    const sendReply = async () => {
+      console.log(reply);
+      await save("messages", reply);
+
+      router.go(-1);
+    };
+
+    onMounted(() => {
+      getMessage();
+    });
+
+    return { message, reply, sendReply };
+  },
+};
+</script>
+
+<style>
+</style>
