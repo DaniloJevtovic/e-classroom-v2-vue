703c23a732472386ae12f50eb7171f6edc83a5d3
Lemur <danilo_jevtovic@hotmail.com>
2021-05-25 11:01:43 +0200

    sredjena komponenta za kviz - izmjenu, ucitana pitanja

--- e-classroom-v2-vue/src/views/quizzes/teacher/QuizDetails.vue
+++ e-classroom-v2-vue/src/views/quizzes/teacher/QuizDetails.vue
@@ -2,209 +2,97 @@
   <div class="container">
     <div class="container-header">
       <button @click="$router.go(-1)">Back to quizzes</button>
-      <!-- <router-link :to="{ name: 'MyCourseDetails', params: { id: id } }">
-        <button>Back to course</button>
-      </router-link> -->
     </div>
     <div class="container-body">
       <div class="quiz-info">
-        <h1>Name: {{ quizInfo.name }}</h1>
-        <h2>Instructions: {{ quizInfo.instructions }}</h2>
-        <h2>Duration: {{ quizInfo.duration }}</h2>
-        <h2>QuizStatus: {{ quizInfo.status }}</h2>
-        <h3>Total questions: {{ questions.length }}</h3>
+        <form @submit.prevent="handleSubmit">
+          <h2>Quiz details</h2>
+          <input type="text" v-model="quiz.name" placeholder="name" required />
+          <textarea
+            rows="3"
+            v-model="quiz.instructions"
+            placeholder="instructions"
+          ></textarea>
 
-        <router-link :to="{ name: 'EditQuiz', params: { quziId: quizId } }">
-          <button>Edit</button>
-        </router-link>
-
-        <button @click="deleteQuiz">Delete</button>
-      </div>
-
-      <hr />
-
-      <div class="questions">
-        <h1>Questions</h1>
-        <!-- <router-link :to="{ name: 'NewQuestion', params: { quizId: quizId } }">
-          <button>New Question</button>
-        </router-link> -->
-
-        <div v-for="question in questions" :key="question.id">
-          <question-details
-            :questionId="question.id"
-            @deleteFromList="deleteQuestion"
-          ></question-details>
-          <hr />
-        </div>
-        <button @click="addNewQuestionForm">New Question 222</button>
-      </div>
-
-      <!-- lista za nova pitanja kad se klikne na dugme da se doda pitanje -->
-      <div
-        class="new-questions-list"
-        v-for="(newQuest, index) in newQuestionsList"
-        :key="newQuest"
-      >
-        <div class="new-question">
-          <form @submit.prevent="saveQuestion(newQuest, index)">
-            <div class="flex-div">
-              <select name="ans-type" v-model="newQuest.questionType" required>
-                <option disabled value="">Select answer type</option>
-                <option value="MULTIPLE_CHOICE">Multiple choice</option>
-                <option value="TRUE_FALSE">True false</option>
-                <option value="FILL">Fill</option>
-              </select>
-              <input
-                type="number"
-                v-model="newQuest.points"
-                placeholder="points"
-                required
-              />
-            </div>
-
+          <div class="status-duration">
             <input
-              type="text"
-              v-model="newQuest.question"
-              placeholder="new question"
+              type="number"
+              v-model="quiz.duration"
+              placeholder="duration"
               required
             />
 
-            <button>Save</button>
-            <button @click.prevent="cancelQuestion(index)">Cancel</button>
-          </form>
-        </div>
+            <select>
+              <option value="Select status">Status</option>
+            </select>
+          </div>
+
+          <button>Save</button>
+          <!-- <button @click.prevent="deleteQuiz(quiz.course.id)">Delete</button> -->
+        </form>
       </div>
+
+      <!-- lista pitanja za kviz -->
+      <questions-list :quizId="quizId"></questions-list>
     </div>
   </div>
 </template>
 
 <script>
-import { ref, onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import useCRUD from "@/composables/useCRUD.js";
-import QuestionDetails from "./QuestionDetails.vue";
 import { useRouter } from "vue-router";
 
+import QuestionsList from "./QuestionsList.vue";
+
 export default {
-  props: ["id", "quizId"],
-  components: { QuestionDetails },
+  props: ["quizId"],
+  components: { QuestionsList },
   setup(props) {
-    const { getById, getSubItems, deleteById, save } = useCRUD();
+    const { getById, editById, deleteById } = useCRUD();
     const router = useRouter();
 
-    const quizInfo = ref("");
-
-    const questions = ref([]);
-
-    const newQuestionsList = ref([]);
+    const quiz = ref("");
 
     const getQuiz = async () => {
-      quizInfo.value = await getById("quizzes", props.quizId);
+      quiz.value = await getById("quizzes", props.quizId);
     };
 
-    const deleteQuiz = async () => {
+    onMounted(getQuiz);
+
+    const handleSubmit = async () => {
+      console.log(quiz.value);
+      await editById("quizzes", props.quizId, quiz.value);
+
+      router.push({ name: "QuizDetails", params: { quizId: props.quizId } });
+    };
+
+    const deleteQuiz = async (courseId) => {
       await deleteById("quizzes", props.quizId);
-      router.push({ name: "MyCourseDetails", params: { id: props.id } });
+      router.push({ name: "MyCourseDetails", params: { id: courseId } });
     };
 
-    const getQuestionsForQuiz = async () => {
-      questions.value = await getSubItems("questions", "quiz", props.quizId);
-    };
-
-    //brisanje iz liste (prikaz) - brisanje na bekendu je u question details komponenti!
-    const deleteQuestion = (id) => {
-      let index = questions.value.findIndex((question) => question.id === id);
-      questions.value.splice(index, 1);
-    };
-
-    onMounted(() => {
-      getQuiz();
-      getQuestionsForQuiz();
-    });
-
-    const addNewQuestionForm = () => {
-      newQuestionsList.value.push({
-        question: "",
-        points: "",
-        quizId: props.quizId,
-        questionType: "",
-      });
-    };
-
-    const saveQuestion = async (question, index) => {
-      console.log(question);
-
-      let res = await save("questions", question);
-
-      //kad kreiram true/false pitanje odmah za njega kreiram i 2 odgovora
-      //jedan tacan a drugi netacan
-      if (res.questionType === "TRUE_FALSE") {
-        await save("answers", {
-          answer: "",
-          correct: true,
-          questionId: res.id,
-        });
-
-        await save("answers", {
-          answer: "",
-          correct: false,
-          questionId: res.id,
-        });
-      }
-
-      //console.log("odg", res);
-      questions.value.push(res);
-
-      // brisem iz liste novih pitanja - jer sam dodao na bekend
-      newQuestionsList.value.splice(index, 1);
-    };
-
-    const cancelQuestion = (index) => {
-      newQuestionsList.value.splice(index, 1);
-    };
-
-    return {
-      quizInfo,
-      questions,
-      deleteQuiz,
-      deleteQuestion,
-      newQuestionsList,
-      addNewQuestionForm,
-      saveQuestion,
-      cancelQuestion,
-    };
+    return { quiz, handleSubmit, deleteQuiz };
   },
 };
 </script>
 
 <style scoped>
 .quiz-info {
-  padding: 14px;
-  border: 7px solid darkblue;
+  padding: 5px;
+  border: 2px solid darkblue;
   background: blue;
-  border-radius: 20px;
+  border-radius: 3px;
 }
 
-.questions {
-  padding: 10px;
-}
-
-.new-question {
-  background: rgb(70, 3, 50);
-  padding: 10px;
+.status-duration {
+  display: flex;
+  gap: 1rem;
 }
 
 form {
-  background: springgreen;
-  min-width: 90%;
-}
-
-input,
-select {
-  margin: 5px;
-}
-
-.flex-div {
-  display: flex;
-  min-width: 100%;
+  background: indigo;
+  max-width: 100%;
+  color: cyan;
 }
 </style>
