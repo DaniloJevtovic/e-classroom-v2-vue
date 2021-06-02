32195665b52dfb8af049cb9f44079adbad3db403
Lemur <danilo_jevtovic@hotmail.com>
2021-06-02 11:04:02 +0200

    novi zadatak

---
+++ e-classroom-v2-vue/src/views/assignments/teacher/NewAssignment.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <form @submit.prevent="handleSubmit">
+      <h2>New Assignment</h2>
+      <input type="text" placeholder="name" />
+      <textarea rows="5" placeholder="instructions"></textarea>
+      <button>Save</button>
+      <button @click.prevent="$router.go(-1)">Cancel</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {},
+};
+</script>
+
+<style>
+</style>
