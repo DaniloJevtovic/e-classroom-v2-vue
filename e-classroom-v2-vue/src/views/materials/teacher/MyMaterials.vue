5955837978aa6d0ca4e785459f3f067e5c8ceb0c
Lemur <danilo_jevtovic@hotmail.com>
2021-07-12 20:33:00 +0200

    izmjene u prikazu materijala kod profesora

--- e-classroom-v2-vue/src/views/materials/teacher/MyMaterials.vue
+++ e-classroom-v2-vue/src/views/materials/teacher/MyMaterials.vue
@@ -1,42 +1,36 @@
 <template>
   <div class="materials-info">
-    <h2>Materials</h2>
+    <h3>Materials</h3>
+
+    <input type="text" placeholder="search by name" />
 
     <router-link :to="{ name: 'NewMaterial', params: { id: id } }">
       <button>New Material</button>
     </router-link>
-
-    <input type="text" placeholder="search" />
-
-    <div class="materials">
-      <div v-for="material in materials" :key="material.id">
-        <!-- <div v-for="(material, index) in materials" :key="material.id"> -->
-        <div class="material">
+  </div>
+  <div>
+    <table>
+      <thead>
+        <td>Num</td>
+        <td>Name</td>
+        <td>Description</td>
+        <td>Date</td>
+        <td>Details</td>
+      </thead>
+      <tr v-for="(material, index) in materials" :key="material.id">
+        <td>{{ index + 1 }}.</td>
+        <td>{{ material.name }}</td>
+        <td>{{ material.description }}</td>
+        <td>{{ material.creationDate }}</td>
+        <td>
           <router-link
             :to="{ name: 'MaterialDetails', params: { matId: material.id } }"
           >
-            <h2>Name: {{ material.name }}</h2>
-            <h3>Description: {{ material.description }}</h3>
+            <button>Details</button>
           </router-link>
-
-          <!-- <h2>Name: {{ material.name }}</h2>
-        <h3>Description: {{ material.description }}</h3>
-        <router-link
-          :to="{ name: 'EditMaterial', params: { id: material.id } }"
-        >
-          <button>Edit</button>
-        </router-link>
-        <router-link
-          :to="{ name: 'MaterialDetails', params: { matId: material.id } }"
-        >
-          <button>Details</button>
-        </router-link>
-        <button @click.prevent="deleteMaterial(material.id, index)">
-          Delete
-        </button> -->
-        </div>
-      </div>
-    </div>
+        </td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -72,25 +66,14 @@ export default {
 
 <style scoped>
 .materials-info {
-  padding: 10px;
+  padding: 5px;
   background: rgb(5, 141, 231);
-}
-
-.materials {
-  color: white;
-  background: deepskyblue;
-  padding: 8px;
-  margin: 10px;
-  border-radius: 10px;
   display: grid;
-  grid-template-columns: 1fr 1fr;
-  border: 1px solid white;
+  grid-template-columns: 1fr 2fr 1fr;
+  align-items: center;
 }
 
-.material {
-  background: white;
-  border-radius: 10px;
-  margin: 4px;
-  padding: 3px;
+input {
+  margin: 0;
 }
 </style>
