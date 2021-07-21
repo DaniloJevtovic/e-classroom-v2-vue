295e8b11b57652dbd643ac2be62c614a5b5ec1f3
Lemur <danilo_jevtovic@hotmail.com>
2021-07-21 21:01:20 +0200

    izmjene u prikazu kvizova za ucenike

--- e-classroom-v2-vue/src/views/quizzes/student/StudentQuizzes.vue
+++ e-classroom-v2-vue/src/views/quizzes/student/StudentQuizzes.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="quizzes-info">
-    <h1>Quizzes</h1>
+    <!-- <h3>Quizzes</h3> -->
 
     <div v-if="quizzes.length">
-      <input type="text" placeholder="search" />
+      <div class="quiz-header">
+        <div></div>
+        <input type="text" placeholder="search by name" />
+        <div></div>
+      </div>
 
-      <div class="st-qq">
-        <div class="quizzes" v-for="quiz in quizzes" :key="quiz.id">
+      <div class="quizzes">
+        <div v-for="quiz in quizzes" :key="quiz.id">
           <div v-if="quiz.quizStatus == 'ACTIVE'">
             <router-link
               :to="{
@@ -15,12 +19,14 @@
               }"
             >
               <div class="st-quiz-det">
-                <h2>Quiz Name: {{ quiz.name }}</h2>
+                <h3>Quiz Name: {{ quiz.name }}</h3>
                 <h3>Instruction: {{ quiz.instructions }}</h3>
                 <h4>Duration: {{ quiz.duration }} minutes.</h4>
-              </div>
 
-              <student-quizzes-ress :quizId="quiz.id"></student-quizzes-ress>
+                <br />
+                <!-- ucitavanje rezultata za kviz -->
+                <student-quizzes-ress :quizId="quiz.id"></student-quizzes-ress>
+              </div>
             </router-link>
           </div>
         </div>
@@ -61,25 +67,43 @@ export default {
 </script>
 
 <style scoped>
-.quizzes-info {
-  padding: 10px;
-  background: orange;
+.quiz-header {
+  padding: 5px;
+  background: rgb(103, 5, 231);
+  display: grid;
+  grid-template-columns: 1fr 2fr 1fr;
+  align-items: center;
 }
 
-.st-quiz-det {
-  padding: 14px;
-  color: cornflowerblue;
+.quizzes-info {
+  background: rgb(245, 243, 241);
 }
 
 .quizzes {
   background: rgb(3, 3, 39);
-  padding: 8px;
+  margin: 3px;
+  padding: 2px;
+}
+
+.st-quiz-det {
   margin: 10px;
+  padding: 14px;
+  color: cornflowerblue;
+  border: 1px solid white;
+}
+
+.quiz {
+  padding: 10px;
+  margin: 1px;
 }
 
 .no-quizzes {
   margin: 20px;
   padding: 20px;
   background: red;
+}
+
+input {
+  margin: 0px;
 }
 </style>
