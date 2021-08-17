c9655d1eecbeea6add5d20f75c2455e1c649ca6e
Lemur <danilo_jevtovic@hotmail.com>
2021-08-17 05:01:53 +0200

    modal za dodavanje novog predmeta

---
+++ e-classroom-v2-vue/src/views/courses/admin/NewCourseModal.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal">
+      <form @submit.prevent="handleSubmit" v-if="teachers.length">
+        <!-- ako nema profesora ili razreda u sistemu nece moci da se kreira predmet -->
+        <h2>New Course</h2>
+        <h2>ScClass: {{ scClass.name }}</h2>
+        <input type="text" v-model="course.name" placeholder="name" required />
+        <textarea
+          v-model="course.description"
+          placeholder="description"
+          rows="3"
+        >
+        </textarea>
+
+        <select name="teachers" v-model="course.teacherId" required>
+          <option disabled value="">Select teacher</option>
+          <option
+            v-for="teacher in teachers"
+            :key="teacher"
+            :value="teacher.id"
+          >
+            {{ teacher.firstName }} {{ teacher.lastName }} - ({{
+              teacher.subjects
+            }})
+          </option>
+        </select>
+
+        <button>Save</button>
+
+        <button @click.prevent="closeModal">Cancel</button>
+      </form>
+      <div v-else class="container">
+        <h2>Nije moguce kreirati predmet bez profesora!</h2>
+        <router-link :to="{ name: 'AllCourses', params: { id: scClassId } }">
+          Back to scClass
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { reactive, ref, onMounted } from "vue";
+import useCRUD from "../../../composables/useCRUD.js";
+
+export default {
+  props: ["scClassId"],
+  setup(props, context) {
+    // vodi racuna da u setup() metodi prvi parametar mora biti props
+    // pa tek onda moze context!!!
+    const { getAll, save, getById } = useCRUD();
+
+    const teachers = ref([]);
+    const scClass = ref("");
+
+    const course = reactive({
+      name: "",
+      description: "",
+      teacherId: "",
+      schoolClassId: props.scClassId,
+    });
+
+    const loadScClass = async () => {
+      scClass.value = await getById("scClasses", props.scClassId);
+    };
+
+    const loadTeachers = async () => {
+      teachers.value = await getAll("teachers");
+    };
+
+    onMounted(() => {
+      loadTeachers();
+      loadScClass();
+    });
+
+    const handleSubmit = async () => {
+      let res = await save("courses", course, false, true);
+      context.emit("dodajUListu", res);
+      closeModal();
+    };
+
+    const closeModal = () => {
+      context.emit("zatvoriModal");
+    };
+
+    return { course, teachers, scClass, handleSubmit, closeModal };
+  },
+};
+</script>
+
+<style scoped>
+</style>
