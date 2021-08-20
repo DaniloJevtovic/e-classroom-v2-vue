92428449210a8655b930b6858a0102fbd3edf638
Lemur <danilo_jevtovic@hotmail.com>
2021-08-20 03:07:09 +0200

    izmjene u detaljima razreda

--- e-classroom-v2-vue/src/views/schoolClasses/SchoolClassDetails.vue
+++ e-classroom-v2-vue/src/views/schoolClasses/SchoolClassDetails.vue
@@ -5,28 +5,44 @@
     </div>
 
     <div class="container-body">
-      <router-link
+      <!-- izmjena razreda - nova komponeta -->
+      <!-- <router-link
         :to="{ name: 'EditSchoolClass', params: { id: scClassInfo.id } }"
       >
         <div class="scClass-admin">
           <h2>ScClass: {{ scClassInfo.name }}</h2>
           <p>Description: {{ scClassInfo.description }}</p>
         </div>
-      </router-link>
+      </router-link> -->
 
+      <div style="color: darkblue" class="scClass-admin" @click="toggleModal">
+        <h2>ScClass: {{ scClassInfo.name }}</h2>
+        <p>Description: {{ scClassInfo.description }}</p>
+      </div>
+
+      <!-- modal za izmjenu razreda -->
+      <edit-sc-modal
+        v-if="showEditScModal"
+        :sc="scClassInfo"
+        @zatvoriModal="toggleModal"
+      >
+      </edit-sc-modal>
+
+      <!-- ruter-view sa child komponete unutar razreda (predmeti, ucenici) -->
       <router-view />
     </div>
   </div>
 </template>
 
 <script>
-import { ref, onMounted } from "vue";
+import { ref, reactive, onMounted } from "vue";
 import useCRUD from "../../composables/useCRUD.js";
 import ChildNavbar from "../../components/ChildNavbar.vue";
+import EditScModal from "./EditScModal.vue";
 
 export default {
   props: ["id"],
-  components: { ChildNavbar },
+  components: { ChildNavbar, EditScModal },
   setup(props) {
     const scClassInfo = ref("");
 
@@ -55,16 +71,23 @@ export default {
       getDetailsSC();
     });
 
-    return { scClassInfo, links };
+    const showEditScModal = ref(false);
+
+    const toggleModal = () => {
+      showEditScModal.value = !showEditScModal.value;
+    };
+
+    return { scClassInfo, links, showEditScModal, toggleModal };
   },
 };
 </script>
 
 <style scoped>
 .scClass-admin {
-  padding: 10px;
+  padding: 5px;
   background: cyan;
-  font-family: 'Courier New', Courier, monospace;
+  font-family: "Courier New", Courier, monospace;
+  cursor: pointer;
 }
 
 .stClasses {
