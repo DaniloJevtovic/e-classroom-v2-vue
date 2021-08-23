e557a26e2c8025f9efda004b7f865d28f3b1117c
Lemur <danilo_jevtovic@hotmail.com>
2021-08-23 23:44:52 +0200

    modal za izmjenu kviza

---
+++ e-classroom-v2-vue/src/views/quizzes/teacher/EditQuizModal.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal">
+      <form @submit.prevent="handleSubmit">
+        <h3>Quiz details</h3>
+        <input
+          type="text"
+          v-model="quizToEdit.name"
+          placeholder="name"
+          required
+        />
+        <textarea
+          rows="4"
+          v-model="quizToEdit.instructions"
+          placeholder="instructions"
+        ></textarea>
+
+        <div class="status-duration">
+          <input
+            type="number"
+            v-model="quizToEdit.duration"
+            placeholder="duration"
+            required
+          />
+
+          <select v-model="quizToEdit.quizStatus" required>
+            <option value="INACTIVE">Inactive</option>
+            <option value="ACTIVE">Active</option>
+            <option value="FINISHED">Finished</option>
+            <option value="DELETED">Deleted</option>
+          </select>
+        </div>
+
+        <button>Save</button>
+        <button @click.prevent="closeModal">Cancel</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { reactive, ref, onMounted } from "vue";
+import useCRUD from "../../../composables/useCRUD.js";
+
+export default {
+  props: ["quiz"],
+  setup(props, context) {
+    const { editById } = useCRUD();
+    const quizToEdit = reactive({
+      name: props.quiz.name,
+      instructions: props.quiz.instructions,
+      duration: props.quiz.duration,
+      quizStatus: props.quiz.quizStatus,
+    });
+
+    const handleSubmit = async () => {
+      let res = await editById(
+        "quizzes",
+        props.quiz.id,
+        quizToEdit,
+        false,
+        true
+      );
+
+      if (res.status != 400) {
+        //update u roditeljskoj komponenti - nekako sirotinjski :/
+        props.quiz.name = quizToEdit.name;
+        props.quiz.instructions = quizToEdit.instructions;
+        props.quiz.duration = quizToEdit.duration;
+        props.quiz.quizStatus = quizToEdit.quizStatus;
+
+        closeModal();
+      }
+    };
+
+    const closeModal = () => {
+      context.emit("zatvoriModal");
+    };
+
+    return { quizToEdit, handleSubmit, closeModal };
+  },
+};
+</script>
+
+<style scoped>
+.backdrop {
+  background: rgba(77, 240, 186, 0.7);
+}
+</style>
