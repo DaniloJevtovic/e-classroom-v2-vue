b0e16f816c40edef5241be1865412531eb96caab
Lemur <danilo_jevtovic@hotmail.com>
2021-08-26 20:17:06 +0200

    izmjena poena na rezultatu - profesor

--- e-classroom-v2-vue/src/views/quizzes/student/res/StudentQuizResults.vue
+++ e-classroom-v2-vue/src/views/quizzes/student/res/StudentQuizResults.vue
@@ -4,13 +4,22 @@
       <!-- <h1 v-if="result.points < 0">Your Score: 0 points / ?</h1>
       <h1 v-else>Your Score: {{ result.points }} points / ?</h1> -->
 
-      <h2>Score: {{ result.points }} points / ?</h2>
-      <button v-if="checkLoggedUser()">Edit points</button>
+      <div v-if="checkLoggedUser() && !edit">
+        <h2>Score: {{ result.points }} points / ?</h2>
+        <button @click="edit = !edit">Edit points</button>
+      </div>
+
+      <div class="edit-points" v-else-if="checkLoggedUser() && edit">
+        <input type="number" v-model="pointsToEdit" />
+        <button @click="updatePoints">Save</button>
+      </div>
     </div>
 
+    <!-- ucenikovi odgovori -->
     <student-res-questions :quizId="result.quiz.id" :resId="result.id">
     </student-res-questions>
 
+    <!-- komentari na kviz -->
     <student-res-comments :resId="result.id"></student-res-comments>
   </div>
 </template>
@@ -18,14 +27,17 @@
 <script>
 import StudentResQuestions from "./StudentResQuestions.vue";
 import StudentResComments from "./StudentResComments.vue";
-
+import { reactive, ref } from "vue";
 import { useStore } from "vuex";
+import useCRUD from "@/composables/useCRUD.js";
 
 export default {
   props: ["result"],
   components: { StudentResQuestions, StudentResComments },
-  setup() {
+  setup(props) {
     const store = useStore();
+
+    const { editById } = useCRUD();
 
     const checkLoggedUser = () => {
       let loggedUser = store.getters["getLoggedUser"];
@@ -38,7 +50,29 @@ export default {
       }
     };
 
-    return { checkLoggedUser };
+    const edit = ref(false);
+
+    const pointsToEdit = ref(props.result.points);
+
+    const updatePoints = async () => {
+      console.log("vrijednost poena:", pointsToEdit.value);
+
+      let res = await editById(
+        "results/updatePoints",
+        props.result.id,
+        pointsToEdit.value,
+        false,
+        true
+      );
+
+      console.log("rezultate", res);
+
+      props.result.points = res.points;
+
+      edit.value = !edit.value;
+    };
+
+    return { checkLoggedUser, edit, updatePoints, pointsToEdit };
   },
 };
 </script>
