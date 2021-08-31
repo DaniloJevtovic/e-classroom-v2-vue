8bcb02fd5f8ac0901a4195425e93d1a9358625bb
Lemur <danilo_jevtovic@hotmail.com>
2021-08-31 22:57:23 +0200

    izdvojeni rezultati u posebnu komponentu

---
+++ e-classroom-v2-vue/src/views/quizzes/student/res/StudentAllResultTable.vue
@@ -0,0 +1,63 @@
+<template>
+  <td>{{ index + 1 }}.</td>
+  <td>{{ result.quiz.name }}</td>
+  <td>{{ result.date }}</td>
+  <td>{{ result.quiz.course.name }}</td>
+  <td>
+    {{ result.quiz.course.teacher.firstName }}
+    {{ result.quiz.course.teacher.lastName }}
+  </td>
+  <td style="background: springgreen">{{ result.points }}</td>
+  <td>
+    <ModalSlot v-show="isModalVisible" @close="closeModal">
+      <template v-slot:header>
+        <h3>Quiz: {{ result.quiz.name }}</h3>
+      </template>
+
+      <!-- prikaz rezultata u modalu -->
+      <template v-slot:body>
+        <student-quiz-results :result="result"></student-quiz-results>
+      </template>
+
+      <template v-slot:footer>
+        <h3>Date: {{ result.date }}</h3>
+        <!-- <span>Solve duration: {{ result.solveDuration }} min</span> -->
+      </template>
+    </ModalSlot>
+
+    <button
+      type="button"
+      class="pill-button"
+      style="background: purple"
+      @click="showModal"
+    >
+      Show result!
+    </button>
+  </td>
+</template>
+
+<script>
+import { ref } from "vue";
+import ModalSlot from "../../../../components/ModalSlot.vue";
+import StudentQuizResults from "./StudentQuizResults.vue";
+
+export default {
+  props: ["result", "index"],
+  components: { ModalSlot, StudentQuizResults },
+  setup() {
+    const isModalVisible = ref(false);
+
+    const showModal = () => {
+      isModalVisible.value = true;
+    };
+    const closeModal = () => {
+      isModalVisible.value = false;
+    };
+
+    return { isModalVisible, showModal, closeModal };
+  },
+};
+</script>
+
+<style>
+</style>
