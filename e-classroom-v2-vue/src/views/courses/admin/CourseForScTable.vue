e8576094ec8c2fba7ab4e621c7499040f950e978
Lemur <danilo_jevtovic@hotmail.com>
2021-08-31 02:44:30 +0200

    komponenta za prikaza podataka o predmetima u tabeli

---
+++ e-classroom-v2-vue/src/views/courses/admin/CourseForScTable.vue
@@ -0,0 +1,72 @@
+<template>
+  <td>{{ index + 1 }}.</td>
+  <td>{{ course.name }}</td>
+  <td>{{ course.description }}</td>
+  <td>{{ course.schoolClass.name }}</td>
+  <td>{{ course.teacher.firstName }} {{ course.teacher.lastName }}</td>
+  <td>
+    <button @click.prevent="toggleMessageModal">
+      Contact teacher
+      <span>&#9993;</span>
+    </button>
+    <div v-if="showNewMessageModal">
+      <NewMessageModal
+        :reciverId="course.teacher.id"
+        @zatvoriModal="toggleMessageModal"
+      >
+      </NewMessageModal>
+    </div>
+
+    <!-- izmjena predmeta - nova komponenta (moze i izmjena razreda) -->
+    <!-- <router-link
+      :to="{
+        name: 'EditCourse',
+        params: { id: course.id },
+      }"
+    >
+      <button>Edit</button>
+    </router-link> -->
+
+    <!-- izmjena predmeta - modal (ne moze promjena razreda) -->
+    <button @click.prevent="toggleEditCourseModal">Edit course</button>
+    <!-- modal za izmjenu predmeta -->
+    <div v-if="showEditCourseModal">
+      <EditCourseModal :course="course" @zatvoriModal="toggleEditCourseModal">
+      </EditCourseModal>
+    </div>
+  </td>
+</template>
+
+<script>
+import { ref } from "vue";
+import EditCourseModal from "./EditCourseModal.vue";
+import NewMessageModal from "../../messages/NewMessageModal.vue";
+
+export default {
+  props: ["course", "index"],
+  components: { EditCourseModal, NewMessageModal },
+  setup() {
+    const showEditCourseModal = ref(false);
+
+    const toggleEditCourseModal = () => {
+      showEditCourseModal.value = !showEditCourseModal.value;
+    };
+
+    const showNewMessageModal = ref(false);
+
+    const toggleMessageModal = () => {
+      showNewMessageModal.value = !showNewMessageModal.value;
+    };
+
+    return {
+      showEditCourseModal,
+      toggleEditCourseModal,
+      showNewMessageModal,
+      toggleMessageModal,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
