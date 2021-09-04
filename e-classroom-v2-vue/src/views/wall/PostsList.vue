dbc495f22c0aedbc3e39fc39e0b1868338fd1253
Lemur <danilo_jevtovic@hotmail.com>
2021-09-04 11:10:38 +0200

    sve za pojedinacnu objavu u posebnoj komponenti

--- e-classroom-v2-vue/src/views/wall/PostsList.vue
+++ e-classroom-v2-vue/src/views/wall/PostsList.vue
@@ -3,88 +3,23 @@
     <div class="new-post">
       <textarea
         rows="5"
-        v-model="post.post"
+        v-model="newPost.post"
         placeholder="type something"
         required
       >
       </textarea>
-      <button @click.prevent="newPost">post</button>
+      <button @click.prevent="addNewPost">post</button>
     </div>
 
     <div v-if="posts.length">
       <div class="post-list">
         <div v-for="(post, index) in posts" :key="post.id">
-          <!-- kada se objava ne mjenja -->
-          <div class="post" v-if="!post.edit">
-            <div class="post-info">
-              <div class="post-text">
-                <p>
-                  {{ post.author.firstName }} {{ post.author.lastName }} / Date:
-                  {{ post.date }}
-                </p>
-                <h3 style="color: cyan">{{ post.post }}</h3>
-                <!-- <p>{{ post.edit }}</p> -->
-              </div>
-
-              <!-- samo autor i profesor modgu da brisu postove -->
-              <div
-                class="post-buttons"
-                v-if="
-                  author.id == post.author.id ||
-                  author.authorities[0].authority == 'ROLE_TEACHER'
-                "
-              >
-                <button @click.prevent="deletePost(index, post.id)">
-                  Delete post
-                </button>
-
-                <!-- samo autor posta moze da mjenja svoj post -->
-                <button
-                  v-if="author.id == post.author.id"
-                  @click="post.edit = !post.edit"
-                >
-                  Edit post
-                </button>
-              </div>
-            </div>
-
-            <!-- lista komentara za post -->
-            <comments-list :postId="post.id"></comments-list>
-          </div>
-
-          <!-- kada se objava edituje -->
-          <div class="post" v-else>
-            <div class="post-edit">
-              <div class="post-text">
-                <p>
-                  {{ post.author.firstName }} {{ post.author.lastName }} / Date:
-                  {{ post.date }}
-                </p>
-                <textarea rows="5" v-model="post.post"></textarea>
-                <!-- <p>{{ post.edit }}</p> -->
-              </div>
-
-              <div
-                class="post-buttons"
-                v-if="
-                  author.id == post.author.id ||
-                  author.authorities[0].authority == 'ROLE_TEACHER'
-                "
-              >
-                <button
-                  @click.prevent="
-                    updatePost(index, post), (post.edit = !post.edit)
-                  "
-                >
-                  Save changes
-                </button>
-
-                <!-- <button @click.prevent="post.edit = !post.edit">Cancel</button> -->
-              </div>
-            </div>
-
-            <!-- lista komentara za post -->
-            <comments-list :postId="post.id"></comments-list>
+          <div class="post">
+            <Post
+              :post="post"
+              :index="index"
+              @obrisiPost="deletePostFromList"
+            />
           </div>
         </div>
       </div>
@@ -97,20 +32,20 @@ import { ref, onMounted, reactive } from "vue";
 import useCRUD from "../../composables/useCRUD.js";
 import { useStore } from "vuex";
 
-import CommentsList from "./CommentsList.vue";
+import Post from "./Post.vue";
 
 export default {
   props: ["courseId"],
-  components: { CommentsList },
+  components: { Post },
   setup(props) {
-    const { getSubItems, save, deleteById, editById } = useCRUD();
+    const { getSubItems, save } = useCRUD();
     const store = useStore();
 
     const author = store.getters["getLoggedUser"];
 
     const posts = ref([]);
 
-    const post = reactive({
+    const newPost = reactive({
       post: "",
       courseId: props.courseId,
       authorId: author.id,
@@ -119,36 +54,20 @@ export default {
     const getPosts = async () => {
       posts.value = await getSubItems("posts", "course", props.courseId);
 
+      //objavama dodjeljum da nisu u statusu izmjene
       posts.value.forEach((post) => {
         post.edit = false;
       });
     };
 
-    const newPost = async () => {
-      let res = await save("posts", post, false, true);
+    const addNewPost = async () => {
+      let res = await save("posts", newPost, false, true);
       posts.value.unshift(res);
-      post.post = "";
+      newPost.post = "";
     };
 
-    const updatePost = async (index, post) => {
-      console.log("prije izmjene", post);
-
-      //konvertujem objavu (iz niza) u odgovarajuci format za bekend (dto)
-      let postToEdit = {
-        post: post.post,
-        courseId: post.course.id,
-        authorId: post.author.id,
-      };
-
-      let res = await editById("posts", post.id, postToEdit, false, true);
-      res.edit = false; //dodajem edit atribut i stavljam mu vrijednost false (isto ono sto sam radio kad sam ucitao sve objave u foreach petlji)
-      posts.value[index] = res; //azuriram vrjednost posta u listi (nakon sto dobijem odgovor sa bekenda)
-      //zbog datuma
-    };
-
-    const deletePost = async (index, id) => {
+    const deletePostFromList = (index) => {
       posts.value.splice(index, 1); //brisanje iz liste
-      await deleteById("posts", id, false, true); //brisanje na bekendu
     };
 
     onMounted(() => {
@@ -156,12 +75,11 @@ export default {
     });
 
     return {
-      post,
-      posts,
       newPost,
-      updatePost,
-      deletePost,
+      posts,
+      addNewPost,
       author,
+      deletePostFromList,
     };
   },
 };
@@ -203,36 +121,5 @@ export default {
   margin: 8px;
   /* border-radius: 7px; */
   padding-top: 20px;
-}
-
-.post-info {
-  border: 1px solid rgb(27, 4, 43);
-  padding: 10px;
-  margin: 0px 10px 4px;
-  background: rgb(36, 8, 102);
-  color: white;
-  display: flex;
-  align-items: center;
-  border-radius: 5px;
-}
-
-.post-edit {
-  border: 1px solid rgb(27, 4, 43);
-  padding: 10px;
-  margin: 0px 10px 4px;
-  background: rgb(36, 8, 102);
-  color: white;
-  align-items: center;
-  border-radius: 5px;
-}
-
-.post-text {
-  text-align: left;
-}
-
-.post-buttons {
-  text-align: right;
-  display: flex;
-  margin-left: auto; /*poravnava u udesno */
 }
 </style>
