641f0695a37b087acd920eb65e9fc94abb0db942
Lemur <danilo_jevtovic@hotmail.com>
2021-09-07 02:45:13 +0200

    izmjestena tabela sa svim predmetima u novu komponentu

--- e-classroom-v2-vue/src/views/courses/admin/AllCourses.vue
+++ e-classroom-v2-vue/src/views/courses/admin/AllCourses.vue
@@ -13,71 +13,14 @@
     </div>
 
     <div class="container-body">
-      <!-- <h3>All Courses</h3> -->
       <div v-if="view">
-        <!-- svi predmeti - nerasporedjeni -->
-        <!-- <div class="courses" v-for="course in courses" :key="course.id">
-          <router-link :to="{ name: 'EditCourse', params: { id: course.id } }">
-            <button>
-              Name: {{ course.name }}
-              <br />
-              Teacher: {{ course.teacher.firstName }}
-              {{ course.teacher.lastName }}
-              <br />
-              School Class: {{ course.schoolClass.name }}
-              <br />
-              Description: {{ course.description }}
-            </button>
-          </router-link>
-        </div> -->
-
         <!-- predmeti razvrstani po razredima -->
         <sc-classes></sc-classes>
       </div>
 
       <div v-else>
         <!-- tabelarni prikaz predmeta -->
-        <table>
-          <thead>
-            <td>#</td>
-            <td>Name</td>
-            <td>Description</td>
-            <td>School Class</td>
-            <td>Teacher</td>
-            <td>Action</td>
-          </thead>
-          <tr v-for="(course, index) in courses" :key="course.id">
-            <!-- td-ovi u posebnoj komponenti -->
-            <CourseForScTable :course="course" :index="index" />
-
-            <!-- <td>{{ index + 1 }}.</td>
-            <td>{{ course.name }}</td>
-            <td>{{ course.description }}</td>
-            <td>{{ course.schoolClass.name }}</td>
-            <td>
-              {{ course.teacher.firstName }} {{ course.teacher.lastName }}
-            </td>
-            <td>
-              <router-link
-                :to="{
-                  name: 'NewMessage',
-                  params: { reciverId: course.teacher.id },
-                }"
-              >
-                <button>Contact teacher</button>
-              </router-link>
-
-              <router-link
-                :to="{
-                  name: 'EditCourse',
-                  params: { id: course.id },
-                }"
-              >
-                <button>Edit</button>
-              </router-link>
-            </td> -->
-          </tr>
-        </table>
+        <AllCoursesTable />
       </div>
     </div>
   </div>
@@ -85,38 +28,18 @@
 
 <script>
 import { ref, onMounted } from "vue";
-import useCRUD from "@/composables/useCRUD.js";
-import { useRouter } from "vue-router";
 import ScClasses from "./ScClasses.vue";
-import CourseForScTable from "./CourseForScTable.vue";
+import AllCoursesTable from "./AllCoursesTable.vue";
 
 export default {
-  components: { ScClasses, CourseForScTable },
+  components: { ScClasses, AllCoursesTable },
   setup() {
-    const router = useRouter();
-    const { getAll } = useCRUD();
-
-    const courses = ref([]);
-    const getCourses = async () => {
-      courses.value = await getAll("courses");
-    };
-
-    onMounted(getCourses);
-
-    return { courses, view: ref(true) };
+    return { view: ref(true) };
   },
 };
 </script>
 
 <style scoped>
-.courses {
-  display: inline;
-}
-
-button {
-  padding: 5px;
-}
-
 input {
   margin: 0;
   width: 75%;
