90e67d499e5f60d3c5174768d8ba46fcd615a56b
Lemur <danilo_jevtovic@hotmail.com>
2021-09-16 02:43:35 +0200

    prikaz predmeta 2

--- e-classroom-v2-vue/src/views/courses/teacher/MyCourse2.vue
+++ e-classroom-v2-vue/src/views/courses/teacher/MyCourse2.vue
@@ -1,12 +1,34 @@
 <template>
+  <div class="container-header"></div>
   <h3>{{ course.name }}</h3>
+
+  {{ posts }}
 </template>
 
 <script>
+import { onMounted, ref } from "vue";
+import useCRUD from "../../../composables/useCRUD.js";
+import ChildNavbar from "../../../components/ChildNavbar.vue";
+
 export default {
-  props: ["course"],
+  props: ["course", "id"],
+  components: { ChildNavbar },
+  setup(props) {
+    const { getSubItems} = useCRUD();
+    const posts = ref([]);
+
+    const getPosts = async () => {
+      posts.value = await getSubItems("posts", "course", props.course.id);
+    };
+
+    onMounted(() => {
+      getPosts();
+    });
+
+    return { posts };
+  },
 };
 </script>
 
-<style>
+<style scoped>
 </style>
