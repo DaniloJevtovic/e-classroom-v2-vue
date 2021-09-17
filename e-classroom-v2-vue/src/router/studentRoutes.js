025b922a9e77c07242f78bcdd72cd23ea3d34001
Lemur <danilo_jevtovic@hotmail.com>
2021-09-17 19:23:57 +0200

    izmjene u putanjama - ucenik

--- e-classroom-v2-vue/src/router/studentRoutes.js
+++ e-classroom-v2-vue/src/router/studentRoutes.js
@@ -75,4 +75,52 @@ export default [
     component: StudentQuizResults,
     props: true,
   },
+
+  {
+    path: "/StudentCourses2",
+    name: "StudentCourses2",
+    component: () => import("../views/courses/student/StudentCourses2.vue"),
+    props: true,
+    children: [
+      {
+        path: "/studentCourse/course/:id",
+        name: "StudentCourseDetails",
+        component: StudentCourseDetails,
+        props: true,
+        redirect: { name: "StudentWall" },
+        children: [
+          {
+            path: "studentWall",
+            name: "StudentWall",
+            component: Wall,
+            props: true,
+          },
+          {
+            path: "materials",
+            name: "StudentMaterials",
+            component: StudentMaterials,
+            props: true,
+          },
+          {
+            path: "quizzes",
+            name: "StudentQuizzes",
+            component: StudentQuizzes,
+            props: true,
+          },
+          {
+            path: "quizzes/quiz/:quizId",
+            name: "StudentQuizDetails",
+            component: StudentQuizDetails,
+            props: true,
+          },
+          {
+            path: "quizzes/quiz/:quizId/solve",
+            name: "StudentSolveQuiz",
+            component: StudentSolveQuiz,
+            props: true,
+          },
+        ],
+      },
+    ],
+  },
 ];
