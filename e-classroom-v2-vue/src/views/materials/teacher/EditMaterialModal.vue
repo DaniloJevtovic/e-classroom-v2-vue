7107e68dcc517fe0c371b437a3a81c07622ab281
Lemur <danilo_jevtovic@hotmail.com>
2021-10-16 19:02:15 +0200

    modal za izmjenu materijala

---
+++ e-classroom-v2-vue/src/views/materials/teacher/EditMaterialModal.vue
@@ -0,0 +1,149 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal">
+      <form @submit.prevent="handleSubmit">
+        <h2>Edit material</h2>
+        <input
+          type="text"
+          v-model="materialToEdit.name"
+          placeholder="Name"
+          required
+        />
+        <textarea
+          rows="3"
+          v-model="materialToEdit.description"
+          placeholder="Description"
+        >
+        </textarea>
+
+        <div class="mat-files">
+          <input type="file" @change="addToList($event)" placeholder="select" />
+          <p v-for="(file, index) in files" :key="file">
+            {{ file.name }}
+            <button @click.prevent="deleteFile(index, file)">delete</button>
+          </p>
+        </div>
+
+        <button>Save</button>
+        <button @click.prevent="closeModal">Cancel</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { onMounted, reactive, ref } from "vue";
+
+import useCRUD from "@/composables/useCRUD.js";
+import useUpDownFile from "@/composables/useUpDownFile.js";
+
+export default {
+  props: ["material"],
+  setup(props, context) {
+    const { editById, getSubItems, deleteById } = useCRUD();
+    const { uploadFile } = useUpDownFile();
+
+    const materialToEdit = reactive({
+      name: props.material.name,
+      description: props.material.description,
+      courseId: props.material.courseId,
+    });
+
+    const files = ref([]);
+
+    const listToAdd = ref([]); //lista fajlova koju cu upload-ujem - novi fajlovi koji ne postoje
+    const listIdsFilesToDelete = ref([]); //lista id-eva fajlova koje trebam obrisati
+
+    const getFilesForMaterial = async () => {
+      files.value = await getSubItems("files", "material", props.material.id);
+    };
+
+    onMounted(() => {
+      getFilesForMaterial();
+    });
+
+    const handleSubmit = async () => {
+      uploadNewFiles(props.material.id);
+      deleteFilesFromBe();
+
+      let res = await editById(
+        "materials",
+        props.material.id,
+        materialToEdit,
+        false,
+        true
+      );
+
+      props.material.name = materialToEdit.name;
+      props.material.description = materialToEdit.description;
+
+      closeModal();
+    };
+
+    const addToList = (event) => {
+      files.value.push(event.target.files[0]);
+      listToAdd.value.push(event.target.files[0]);
+    };
+
+    const deleteFile = (index, file) => {
+      console.log("index:", index);
+      console.log("id: ", file.id);
+      //ako fajl postoji na bekendu - nije dodavan u ovu listu za dodavanje
+      if (file.id != undefined) {
+        files.value.splice(index, 1);
+        listIdsFilesToDelete.value.push(file.id);
+      } else {
+        //ako fajl ne postoji na bekendu vec samo u listi za dodavanje
+        files.value.splice(index, 1);
+
+        //indeki files i listToAdd NISU ISTI!
+
+        let indOfListToAdd = listToAdd.value.indexOf(file);
+        console.log("ssss:", indOfListToAdd);
+        
+        if (indOfListToAdd !== -1) {
+          listToAdd.value.splice(indOfListToAdd, 1); //obrisati ga iz liste za dodavanje
+        }
+      }
+    };
+
+    const uploadNewFiles = async (matId) => {
+      console.log(matId);
+
+      for (let i = 0; i < listToAdd.value.length; i++) {
+        let formData = new FormData();
+        formData.append("file", listToAdd.value[i], listToAdd.value[i].name);
+
+        await uploadFile(matId, formData);
+      }
+    };
+
+    const deleteFilesFromBe = async () => {
+      for (let i = 0; i < listIdsFilesToDelete.value.length; i++) {
+        await deleteById("files", listIdsFilesToDelete.value[i]);
+      }
+    };
+
+    const closeModal = () => {
+      context.emit("zatvoriModal");
+    };
+
+    return {
+      materialToEdit,
+      handleSubmit,
+      files,
+      addToList,
+      deleteFile,
+      closeModal,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.mat-files {
+  border: 1px solid indigo;
+  padding: 8px;
+  margin-bottom: 10px;
+}
+</style>
